6a318b8993b6974d9c43e186d2cc3d19a0271092
freinIsCool <tymontgm@gmail.com>
2026-09-16 19:25:33 +0200

    Initial commit

---
+++ src/client/java/com/frein/adpfurnc/client/AdaptiveSmeltingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.frein.adpfurnc.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AdaptiveSmeltingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
