8614644ebd9b261468dbbe78329c8b0b3bf12c8d
freinIsCool <tymontgm@gmail.com>
2026-09-16 20:31:43 +0200

    add LOGGER class and startup message

--- src/client/java/com/frein/adpfurnc/client/AdaptiveSmeltingClient.java
+++ src/client/java/com/frein/adpfurnc/client/AdaptiveSmeltingClient.java
@@ -1,10 +1,15 @@
 package com.frein.adpfurnc.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class AdaptiveSmeltingClient implements ClientModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("AdaptiveSmelting");
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		LOGGER.info("adapting the furnaces..");
 	}
 }
